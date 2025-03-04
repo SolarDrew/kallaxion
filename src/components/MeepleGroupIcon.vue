ef7c0a4b2305eef32ed66233b0eb1bbc43363737
Drew Leonard <andy.j.leonard@gmail.com>
2025-03-04 14:00:27 +0000

    Need the image file for meeple group as well

---
+++ src/components/MeepleGroupIcon.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import svg from '../assets/meeple-group.svg?raw'
+
+  var meeplesvg = new window.DOMParser().parseFromString(svg, 'image/svg+xml')
+  meeplesvg.documentElement.setAttribute('width', "25px")
+  meeplesvg.documentElement.setAttribute('height', "25px")
+</script>
+
+<template>
+  <div v-html="meeplesvg.documentElement.outerHTML"/>
+</template>
+
+<style scoped>
+</style>
