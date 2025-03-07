e3244a4defcbddd32a302278f8580d7c76df7d0e
Drew Leonard <andy.j.leonard@gmail.com>
2025-03-07 23:01:12 +0000

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify';
 import App from './App.vue'
 import { Row, Column, Hidden } from 'vue-grid-responsive';
 
+const vuetify = createVuetify({})
 const app = createApp(App);
+app.use(vuetify)
 app.component('row', Row);
 app.component('column', Column);
 app.component('hidden', Hidden);
