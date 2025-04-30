6d3fcf7a7d1c4506659c546b35ca81cd691c9f62
Drew Leonard <andy.j.leonard@gmail.com>
2025-04-30 20:19:55 +0000

    Set up PrimeVue to use fancy components

--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createVuetify } from 'vuetify';
-import App from './App.vue'
 import { Row, Column, Hidden } from 'vue-grid-responsive';
+
+import PrimeVue from 'primevue/config';
+
+import App from './App.vue';
 
 const vuetify = createVuetify({})
 const app = createApp(App);
+
 app.use(vuetify)
 app.component('row', Row);
 app.component('column', Column);
 app.component('hidden', Hidden);
+app.use(PrimeVue)
+
 app.mount('#app')
